98af3eb49f163ba9b24c0cc96069ef54d42978d1
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-02-11 10:10:53 +0000

    First approach

---
+++ delete-files.ps1
@@ -0,0 +1,19 @@
+function Delete-FilesParallel {
+    param(
+        [string]$Path
+    )
+
+    Get-ChildItem $Path -File | ForEach-Object {
+        Start-Job {
+            Remove-Item $_.FullName
+        }
+    }
+
+    while (Get-Job | Where-Object { $_.State -eq 'Running' }) {
+        Start-Sleep -Seconds 1
+    }
+
+    Get-Job | Remove-Job
+}
+
+Delete-FilesParallel -Path "C:\path\to\folder"
